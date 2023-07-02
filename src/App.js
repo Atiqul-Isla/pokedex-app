7326d7eb20dd84f8ae49c0985ccf1e89a601c635
Atiqul Islam <isla.atiqul@gmail.com>
2023-07-01 21:20:34 -0400

    Fixed minor issue with App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import './app.css'
+import './App.css'
 import {getPokemonList, getPokemonDescription, getPokemonSpriteUrl} from './utils'
 // counter
 
